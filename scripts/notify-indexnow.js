b5b3c73661add5be8a45399d3afab2a07e0ab190
porteruyo <porteruyo@gmail.com>
2025-07-01 17:02:05 +0700

    Update notify-indexnow.js

--- scripts/notify-indexnow.js
+++ scripts/notify-indexnow.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename); // 'path' is used here
 
 // --- Konfigurasi Anda ---
 const YOUR_DOMAIN = 'https://ngentotpenjagawarung.pages.dev'; // Ganti dengan domain Anda yang sebenarnya
-const API_KEY_NAME = '96230af2-6598-4401-9595-e4b082668d36'; // Ganti dengan GUID Anda
+const API_KEY_NAME = 'b47aff80-9918-4566-9c68-d850c21b8a22'; // Ganti dengan GUID Anda
 const API_KEY_LOCATION = `${YOUR_DOMAIN}/${API_KEY_NAME}.txt`;
 const INDEXNOW_ENDPOINT = 'https://api.indexnow.org/IndexNow';
 // --- Akhir Konfigurasi ---
